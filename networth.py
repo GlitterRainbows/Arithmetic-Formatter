4e957b6a5855d5a1983deb8a2daae293ca2011c3
GlitterRainbows <62261224+GlitterRainbows@users.noreply.github.com>
2022-07-19 13:07:35 +0100

    Add files via upload

---
+++ networth.py
@@ -0,0 +1,21 @@
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': '{subscription key}',
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters
+    'MinDate': '{string}',
+})
+
+try:
+    conn = http.client.HTTPSConnection('api.etoro.com')
+    conn.request("GET", "/API/User/V1/{Username}/Copiers/History?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
